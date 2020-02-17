c4de0087771ecf94564688d9f197977a1c2aa406
Michael Olawuni <olawuni.michael@gmail.com>
2020-02-17 15:44:20 +0100

    Airline reservation system created by Michael Olawuni

---
+++ AirlineSystem/airlinesystem_flight_passengers.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: airlinesystem
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `flight_passengers`
+--
+
+LOCK TABLES `flight_passengers` WRITE;
+/*!40000 ALTER TABLE `flight_passengers` DISABLE KEYS */;
+INSERT INTO `flight_passengers` VALUES (1,'Michael','Olawuni','olawuni.michael@gmail.com','07030752301','mypassword','No. 2 Abuja Stree, Abuja, Nigeria'),(2,'Sunday','Amin','kelly.eze@gmail.com','08030752301','password123','No. 3 Gongola Stree, Kano, Nigeria'),(3,'Ridwan','Ola','riddy@gmail.com','09031752301','2password','No. 1 Abuja Street, Kaduna, Nigeria'),(4,'Testing','Test','test.testing@gmail.com','08001752301','testPassword','No. 2 Abuja Str, Lagos, Nigeria'),(5,'Michael','Olawuni','olawuni.michael@gmail.com','07030752301','mypassword','No. 2 Abuja Stree, Abuja, Nigeria'),(6,'Kelvin','Eze','kelly.eze@gmail.com','08030752301','password123','No. 3 Gongola Stree, Kano, Nigeria'),(7,'Ridwan','Ola','riddy@gmail.com','09031752301','2password','No. 1 Abuja Street, Kaduna, Nigeria'),(8,'Testing','Test','test.testing@gmail.com','08001752301','testPassword','No. 2 Abuja Str, Lagos, Nigeria');
+/*!40000 ALTER TABLE `flight_passengers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-17 15:38:35
